3a9054c9a737510881cb4e3becf90c9458f39b91
jhonny1997str <167597745+jhonny1997str@users.noreply.github.com>
2024-10-28 16:26:33 -0500

    Desarrollo

--- src/main/java/com/JavaJenkins/controlador/ClienteControlador.java
+++ src/main/java/com/JavaJenkins/controlador/ClienteControlador.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteControlador {
+    @Autowired
     private final ClienteServicio clienteServicio;
 
 
--- src/main/java/com/JavaJenkins/service/ClienteServicioImp.java
+++ src/main/java/com/JavaJenkins/service/ClienteServicioImp.java
@@ -3,6 +3,7 @@ package com.JavaJenkins.service;
 import com.JavaJenkins.entidad.Cliente;
 import com.JavaJenkins.repositorio.ClienteRepositorio;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class ClienteServicioImp implements  ClienteServicio{
+    @Autowired
     private final ClienteRepositorio clienteRepositorio;
 
     @Override
